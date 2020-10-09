d02226a79f4f9457922e6351f839d3336476122e
LeeWannacott <wannacottL@gmail.com>
2020-10-10 08:07:08 +1300

    migrate.p

--- tweet_app/tweet_app/settings.py
+++ tweet_app/tweet_app/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    
 ]
 
 MIDDLEWARE = [
