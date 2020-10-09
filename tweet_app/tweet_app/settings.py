cd91b886a59642be545bba502ce8c35a6b3a5056
LeeWannacott <wannacottL@gmail.com>
2020-10-10 08:18:03 +1300

    Add requirements

--- tweet_app/tweet_app/settings.py
+++ tweet_app/tweet_app/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+   
     
 ]
 
