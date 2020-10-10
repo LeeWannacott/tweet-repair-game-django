a4d7e92d81b6a7216e349acf6f342d8c21820d5b
LeeWannacott <wannacottL@gmail.com>
2020-10-10 17:17:52 +1300

    add procfile

--- tweet_app/tweet_app/asgi.py
+++ tweet_app/tweet_app/asgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-import os
+#import os
 
-from django.core.asgi import get_asgi_application
+#from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweet_app.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweet_app.settings')
 
-application = get_asgi_application()
+#application = get_asgi_application()
--- tweet_app/tweet_app/wsgi.py
+++ tweet_app/tweet_app/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
-from django.core.wsgi import get_wsgi_application
+#from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweet_app.settings.py')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweet_app.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
